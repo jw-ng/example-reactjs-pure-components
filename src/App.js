3918c3a725303ffd6fbe2c187e742c6e272515b6
Jun Wei <ngjunwei.15@gmail.com>
2019-09-17 11:27:37 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React from 'react';
-import TodoList from './components/TodoList/TodoList';
-import PureTodoList from './components/PureTodoList/PureTodoList';
+import AppRouter from './components/AppRouter/AppRouter';
 
 import styles from './App.module.css';
 
 const App = () => {
   return (
     <div className={styles.app}>
-      <h4>Todo list:</h4>
-      <TodoList />
-      <hr />
-      <h4>Todo list of "pure components":</h4>
-      <PureTodoList />
+      <AppRouter />
     </div>
   );
 };
