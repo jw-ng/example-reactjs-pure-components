329ed388db5d8c348603135c6e33e6753722c96d
Jun Wei <ngjunwei.15@gmail.com>
2019-09-17 11:25:05 +0800

    Update AppRouter.js

--- src/components/AppRouter/AppRouter.js
+++ src/components/AppRouter/AppRouter.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import LandingPage from '../LandingPage/LandingPage';
 import TodoList from '../TodoList/TodoList';
 import PureTodoList from '../PureTodoList/PureTodoList';
 import MemoizedTodoList from '../MemoizedTodoList/MemoizedTodoList';
@@ -7,19 +8,7 @@ import MemoizedTodoList from '../MemoizedTodoList/MemoizedTodoList';
 const AppRouter = props => (
   <Router>
     <Switch>
-      <Route exact path="/" render={
-        <ul>
-          <li>
-            <Link to="/wasteful">Non-optimised version</Link>
-          </li>
-          <li>
-            <Link to="/pure">React.PureComponent version</Link>
-          </li>
-          <li>
-            <Link to="/memoized">React.memo version</Link>
-          </li>
-        </ul>
-      } />
+      <Route exact path="/" render={_ => <LandingPage />} />
       <Route exact path="/wasteful" render={_ => <TodoList />} />
       <Route exact path="/pure" render={_ => <PureTodoList />} />
       <Route exact path="/memoized" render={_ => <MemoizedTodoList />} />
